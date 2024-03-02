63af703b621b1925bb0762bc61c3f3e7e0b951e5
jaysi <jaysi@kali.jaysi>
2024-03-02 18:48:42 +0530

    learned about conditional rendering

--- first-app/src/Components/ConditionalRendering.jsx
+++ first-app/src/Components/ConditionalRendering.jsx
@@ -1,18 +1,53 @@
 import React from 'react'
 import { useState } from 'react';
 const ConditionalRendering = () => {
-    const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [mapvalue, setMapvalue] = useState([
+    {
+      id: 1,
+      title: "Item 1",
+      des: "Desc 1",
+    },
+    {
+      id: 2,
+      title: "Item 2",
+      des: "Desc 2",
+    },
+    {
+      id: 3,
+      title: "Item 3",
+      des: "Desc 3",
+    },
+    {
+      id: 4,
+      title: "Item 4",
+      des: "Desc 4",
+    }
+  ])
+
+ 
+
+
   return (
     <>
-    <div>
-        {toggle?<button>This is True</button>: <button>This is False</button>}
+      <div>
+        {toggle ? <button>This is True</button> : <button>This is False</button>}
 
         {toggle && <button>This is True</button>}
-<br />
-<br />
-<br />
-        <button onClick={()=>setToggle(!toggle)}>toggle</button>
-    </div>
+        <br />
+        <br />
+        <br />
+        <button onClick={() => setToggle(!toggle)}>toggle</button>
+
+        <div className="list">List rendering</div>
+        {mapvalue.map(value=>{
+          return <div className="card">
+            <h1>{value.id}</h1>
+            <p>{value.title}</p>
+            <p>{value.des}</p>
+          </div>
+        })}
+      </div>
     </>
   )
 }
